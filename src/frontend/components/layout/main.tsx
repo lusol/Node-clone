a59b6f5c3139af18b8bcc0b93773ce11b891bda1
lusol <brian.wang19@gmail.com>
2024-03-01 10:44:23 -0500

    chat nits

--- src/frontend/components/layout/main.tsx
+++ src/frontend/components/layout/main.tsx
@@ -43,6 +43,7 @@ const Main = {
   MainWrapper: Styled.div`
     display: flex;
     width: 80%;
+    max-width: 850px;
     flex-grow: 1;
     border-radius: 30px;
     border: 5px solid ${(props) => props.theme.colors.hunter};
